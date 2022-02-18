94a0c289c3f2872cc4ace659cda03ec5e3bb8384
khadim Niass <niassssn@gmail.com>
2022-02-18 12:41:43 +0000

    exercice 1 terminer, terminer.

--- TP1/scrypt.js
+++ TP1/scrypt.js
@@ -1,41 +1,58 @@
 //initiatlisation
 const btnajouter = document.querySelector("#ajouter");
-const sorti=document.querySelector('.textarea-items');
-
+const sorti = document.querySelector(".textarea-items");
+const btndelete = document.querySelector("#corbeille");
+const btnedit = document.querySelector("#edit");
 //
-function builer(){
-    var grandiv=document.createElement('div');
-    grandiv.class='header-area';
-    var  mydivh=document.createElement('div');
-    mydivh.class = 'header';
+function createur() {
+  var grandiv = document.createElement("div");
+  grandiv.className = "header-area";
+  var mydivh = document.createElement("div");
+  mydivh.className = "header";
 
-    var imgedit=document.createElement('img');
-    imgedit.src= 'imgs/edit1.png';
-    imgedit.id= 'edit';
-    imgedit.alt= 'edit';
-    const mydivspace=document.createElement('div');
-    
-    var imgecorbeille=document.createElement('img');
-    imgecorbeille.src='imgs/corbeille.png';
-    imgecorbeille.id='corbeille';
-    imgecorbeille.alt='corbeille';
-    mytextarea=document.createElement('textarea');
-    var divitem=document.createElement('div');
-    divitem.class="item";
+  var imgedit = document.createElement("img");
+  imgedit.src = "imgs/edit1.png";
+  imgedit.id = "edit";
+  imgedit.width = "20";
+  imgedit.alt = "edit";
+  var btnedit = document.createElement("button");
+  btnedit.appendChild(imgedit);
 
-    mydivh.appendChild(imgedit);
-    mydivh.appendChild(mydivspace);
-    mydivh.appendChild(imgecorbeille);
-    grandiv.appendChild(mydivh);
-    divitem.appendChild(mytextarea);
-    grandiv.appendChild(divitem);
-    grandiv.appendChild(mydivh);
-    
-    sorti.appendChild(grandiv);
-    
+  var imgecorbeille = document.createElement("img");
+  imgecorbeille.src = "imgs/corbeille.png";
+  imgecorbeille.id = "corbeille";
+  imgecorbeille.alt = "corbeille";
+  imgecorbeille.width = "15";
+  var btncorb = document.createElement("button");
+  btncorb.appendChild(imgecorbeille);
+
+  mytextarea = document.createElement("textarea");
+  // mytextarea.setAttribute('rows','10');
+  mytextarea.rows = "10";
+  var divitem = document.createElement("div");
+  divitem.className = "item";
+  mydivh.appendChild(btnedit);
+  mydivh.appendChild(btncorb);
+  grandiv.appendChild(mydivh);
+  divitem.appendChild(mytextarea);
+  grandiv.appendChild(mydivh);
+  grandiv.appendChild(divitem);
+  sorti.appendChild(grandiv);
+
+  //confirmer avant d'effacer
+  btncorb.addEventListener("click", function () {
+    btncorb.parentNode.parentNode.remove();
+  });
+
+  btnedit.addEventListener("click", function () {
+    mytextarea.toggleAttribute('disabled');
+  });
 }
 
-// 
+
+
+
+//
 btnajouter.addEventListener("click", function (e) {
-  builer();
+  createur();
 });
